bf73262b20671ae2ec3cf2880d464b318ea1ea23
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-10 20:43:16 +0200

    Add destination file for map during defend phase

---
+++ src/main/java/SelfDefenceGame.java
@@ -0,0 +1,33 @@
+package src.main.java;
+
+import javax.swing.*;
+
+/**
+ * Responsible for maintaining game running and calling methods.
+ */
+public class SelfDefenceGame {
+    private static JFrame frame;
+
+    private static void setup() {
+        SwingUtilities.invokeLater(() -> {
+            frame = new JFrame("Self-defence Game");
+            JPanel startMap = new Map();
+
+            frame.add(startMap);
+            Map.buildMap("map_defend.txt");
+
+
+            frame.setSize(600, 600);
+            frame.setResizable(false);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+        });
+    }
+
+
+    public static void main(String[] args) {
+        setup();
+    }
+    
+}
