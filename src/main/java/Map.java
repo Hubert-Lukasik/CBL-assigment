fbd893aea03fc6b7f9559af53f6d09d411ff9dd8
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-10 19:43:09 +0200

    Add overriden painComponent

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,10 +1,13 @@
 package src.main.java;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -57,10 +60,46 @@ public class Map extends JPanel {
         return obtainedMap;
     }
 
+    /**
+    * Take filename of the file with mape and change it to list of tiles (.png format) to draw.
+    * @param filename - name of the file containing map
+    */
+    public static void buildMap(String filename) {
+        ArrayList<ArrayList<String>> mapDescription = Map.readMap(filename);
+
+        tilePaths.clear();
+        tileX.clear();
+        tileY.clear();
+
+        for (int row = 0; row < mapDescription.size(); ++row) {
+            for (int column = 0; column < mapDescription.get(row).size(); ++column) {
+
+                // '-'acts as an information that no tile should be placed in particular place 
+                if (mapDescription.get(row).get(column).equals("-")) {
+                    continue;
+                }
+                
+                String filePath;
+                File file = new File("files", mapDescription.get(row).get(column) + ".png");
+                
+                if (!file.exists()) {
+                    continue;
+                }
+
+                filePath = file.getAbsolutePath();
+                System.out.println(filePath);
+
+
+                tilePaths.add(filePath);
+                tileX.add(column * TILE_WIDTH);
+                tileY.add(row * TILE_HEIGHT);
+            }
+        }
+    }
 
     //DRAWING SECTION
 
-    private static ArrayList<String> tileNames = new ArrayList<String>();
+    private static ArrayList<String> tilePaths = new ArrayList<String>();
     private static ArrayList<Integer> tileX = new ArrayList<Integer>();
     private static ArrayList<Integer> tileY = new ArrayList<Integer>();
    
@@ -68,49 +107,21 @@ public class Map extends JPanel {
     private static final int TILE_HEIGHT = 40;
 
        
-    /** 
-    * First.
-    * TODO : write drawing tiles for all tiles in ArrayLists.
-    * END TODO  
-    * 
-    */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-    }
-    
 
-    /**
-    * Take description of the map and change it to list of tiles to draw.
-    * Use readMap method to get required input type 
-    * @param mapDescription - ArrayList of ArrayList containing Strings describing tiles to draw
-    */
-    public static ArrayList<String> buildMap(ArrayList<ArrayList<String>> mapDescription) {
-        
-        tileNames.clear();
-        tileX.clear();
-        tileY.clear();
+        for (int i = 0; i < tilePaths.size(); ++i) {
+            java.awt.image.BufferedImage image = new BufferedImage(1, 1, 1);
 
-        for (int row = 0; row < mapDescription.size(); ++row) {
-            for (int column = 0; column < mapDescription.get(row).size(); ++column) {
-                if (mapDescription.get(row).get(column) == "-") {
-                    continue;
-                }
-
-                tileNames.add(mapDescription.get(row).get(column) + ".png");
-                tileX.add(column * TILE_WIDTH);
-                tileY.add(row * TILE_HEIGHT);
+            try {
+                image = ImageIO.read(new File(tilePaths.get(i)));
+            } catch (IOException e) {
+                System.err.println("Something wrong happend while reading image");
             }
-        } 
 
-        ArrayList<String> res = new ArrayList<String>();
+            g.drawImage(image, tileX.get(i), tileY.get(i), this);
 
-        for (int i = 0; i < tileNames.size(); ++i) {
-            res.add(tileNames.get(i));
-            res.add(String.valueOf(tileX.get(i)));
-            res.add(String.valueOf(tileY.get(i)));
         }
-
-        return res;
-    } 
+    }
 }
