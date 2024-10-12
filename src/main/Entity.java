3f3e52e0a00dfa9dff571468162fe985c2c1b42e
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-12 13:45:18 +0200

    Add first version of Entity class

--- src/main/Entity.java
+++ src/main/Entity.java
@@ -1,11 +1,12 @@
 package src.main;
 
+import static org.junit.Assert.fail;
+
 /**
  * Class defines all methods for mechanics shared by all entities in the game.
  */
 public abstract class Entity {
     private int healthPoints = 0;
-
 
     /**
      * Initialise amount of health points the entity has.
@@ -18,6 +19,24 @@ public abstract class Entity {
     public int getHealthPoints() {
         return healthPoints; 
     }
+
+    /**
+     * Change health points of the entity.
+     * @param damage - damage taken (negavite damage increases hps)
+     */
+    public void takeDamage(int damage) {
+        healthPoints = healthPoints + 1;
+    }
+
+    public boolean isDead() {
+        return false;
+    }
+
+    
+
+
+
+
 
     
 }
