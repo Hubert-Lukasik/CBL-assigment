eecf1e46cee97a2ade389615aefaba332c7735b1
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-13 21:00:11 +0200

    Change main game loop to show test player character

--- src/main/java/SelfDefenceGame.java
+++ src/main/java/SelfDefenceGame.java
@@ -7,26 +7,28 @@ import javax.swing.*;
  */
 public class SelfDefenceGame {
     private static JFrame frame;
+    private static Player player;
 
     private static void setup() {
-        SwingUtilities.invokeLater(() -> {
-            frame = new JFrame("Self-defence Game");
-            JPanel startMap = new Map();
-
-            frame.add(startMap);
-            Map.buildMap("map_defend.txt");
-
-
-            frame.setSize(600, 600);
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            
-        });
+        Map.buildMap("map_defend.txt");
+        player = new Player();
+        player.setImage("test_char");
+        player.setPosition(200, 200);
     }
-
 
     public static void main(String[] args) {
         setup();
+        SwingUtilities.invokeLater(() -> {
+            frame = new JFrame("Self-defence Game");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(600, 600);
+
+            Painter gamePanel = new Painter();
+            gamePanel.informAboutPlayer(player);
+
+            frame.add(gamePanel);
+
+            frame.setVisible(true);
+        });
     }
-    
 }
