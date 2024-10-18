9827f4ef3979bb8d6b50399c27e9968abf39c6cb
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-18 15:47:17 +0200

    Add basic functionality of phases

--- src/main/java/Phases.java
+++ src/main/java/Phases.java
@@ -1,17 +1,23 @@
 package src.main.java;
 
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*; 
+
 /**
  * Class contains methods and variables connected to managing in-game phases.
  */
-public class Phases {
+public class Phases implements ActionListener {
     private static String currentPhase = "";
-    private static long level = 0; 
+    private static long level = 0;
+    private static Timer planPhaseTimer; 
+    private static Random randomGen = new Random();
 
-    public static String getPhase() {
+    public String getPhase() {
         return currentPhase;
     }
 
-    public static long getLevel() {
+    public long getLevel() {
         return level;
     }
 
@@ -19,20 +25,20 @@ public class Phases {
     * Set level to new non-negative value.
     * @param l - new valuer for level  
     */
-    public static void setLevel(Long l) {
+    public void setLevel(Long l) {
         if (l >= 0L) {
             level = l;
         }
     }
 
-    public static void increaseLevel() {
+    public void increaseLevel() {
         level += 1L;
     }
 
     /**
-     * Start the "Defend" phase when enemiec appears.
+     * Start the "Defend" phase when enemies appears.
      */
-    public static void startDefendPhase() {
+    public void startDefendPhase() {
         currentPhase = "Defend";
         for (long i = 0; i < level; ++i) {
             Opponent.addOpponent();
@@ -40,12 +46,25 @@ public class Phases {
     }
 
     /**
-     * Start the "Plan" phase when enemiec appears.
+     * Start the "Plan" phase when enemies are defeated.
      */
-    public static void startPlanPhase() {
+    public void startPlanPhase() {
         currentPhase = "Plan";
         Opponent.clearOpponentsArray();
         level += 1;
+        planPhaseTimer = new Timer(2000 + randomGen.nextInt(1000), this);
+        planPhaseTimer.start();
     }
+
+    /**
+    * End "Plan" phase.
+    */
+    public void actionPerformed(ActionEvent t) {
+        if (t.getSource() == planPhaseTimer) {
+            planPhaseTimer.stop();
+            startDefendPhase();
+        }
+    }
+
     
 }
