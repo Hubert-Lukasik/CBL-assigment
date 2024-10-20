5970a3fc4d4d5fa1e2def5158f462bc010fe50a5
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-20 20:06:20 +0200

    Make shop appear in Plan pahse and disappear in Defend phase

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,12 +1,14 @@
 package src.main.java;
 
 import java.awt.*;
+import javax.swing.*;
 
 /**
  * Contains methods and variables relevant for in-game shop.
  */
 public class Shop {
     private boolean isShown;
+    private JButton buyTurretButton;
 
     public boolean isShown() {
         return this.isShown;
@@ -16,24 +18,44 @@ public class Shop {
         isShown = false;
     }
 
+    /**
+     * Let the program know shop should be shown.
+     */
     public void showShop() {
         isShown = true;
+        buyTurretButton = new JButton(new ImageIcon("files/turretTest.png"));
     }
 
+    /**
+     * Constructor for Shop class.
+     */
     public Shop() {
         isShown = false;
+
     }
 
     /**
      * Drawing method for shop.
      * @param g - used by Swing
      */
-    public void draw(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g; 
-        Stroke str = new BasicStroke(4f);
-        g2d.setStroke(str);
-        g2d.setColor(Color.GREEN);
-        g2d.drawLine(0, Constants.getMapHeight(), 
-            Constants.getMapWidth(), Constants.getMapHeight());
+    public void draw(Graphics g, Painter p) {
+        System.out.println(this.isShown());
+        if (this.isShown()) {
+            Graphics2D g2d = (Graphics2D) g; 
+
+            //draw line
+            Stroke str = new BasicStroke(4f);
+            g2d.setStroke(str);
+            g2d.setColor(Color.GREEN);
+            g2d.drawLine(0, Constants.getMapHeight(), 
+                Constants.getMapWidth(), Constants.getMapHeight());
+        
+            buyTurretButton.setBounds(20, Constants.getMapHeight(), 
+                20 + Constants.getTurretButtonWidth(), 
+                    Constants.getTileWidht() + Constants.getTurretButtonHeight());
+            p.add(buyTurretButton);
+        } else {
+            p.remove(buyTurretButton);
+        }
     }
 }
