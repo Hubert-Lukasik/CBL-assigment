46cdd35dc765ac4b091053515aa6b933659817aa
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-20 12:39:15 +0200

    Add methods: giveCurrency, getCurrency(), buy()

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -6,7 +6,31 @@ import java.awt.*;
  * Class containt all mechanics related to the player character.
  */
 public class Player extends Entity {
+    
+    private long currency = 0;
 
+    public void giveCurrency(long x) {
+        this.currency += x;
+    }
+
+    public long getCurrency() {
+        return this.currency;
+    }
+
+    /**
+     * Checks whether an item for x amount of currency can be bought.
+     * @param x - cost of the item
+     * @return - true if player can buy item and deduce the cost, return false otherwise
+     */
+    public boolean buy(long x) {
+        if (x <= currency) {
+            this.giveCurrency(-x);
+            return true;
+        }
+
+        return false;
+    }
+   
     /**
      * Draw Player character.
      * @param g - used by Swing
