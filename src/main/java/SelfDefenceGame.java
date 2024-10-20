7bcbfc66231226e2ab1a29f9f7cb088ac39d1143
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-20 18:54:25 +0200

    Increase readability of the code

--- src/main/java/SelfDefenceGame.java
+++ src/main/java/SelfDefenceGame.java
@@ -8,37 +8,41 @@ import javax.swing.*;
  */
 public class SelfDefenceGame {
     private static JFrame frame;
-    private static Player player;
     private static Painter gamePanel;
+    private static Map map;
+    private static Player player;
+    private static Shop shop;
+    private static Phases phasesManager;
+    private static PlayerAnimation playerAnimator;
 
     private static void setup() {
         //Define game map
-        Map map = new Map();
+        map = new Map();
         map.buildMap("map_defend.txt");
         
         //Define player instance
         player = new Player("player_s", 200, 200);
 
         //Define shop
-        Shop shop = new Shop();
+        shop = new Shop();
 
         //Define phases manager
-        Phases phasesManager = new Phases(shop);
+        phasesManager = new Phases(shop);
 
         phasesManager.startDefendPhase();
 
         //Define Painter instance, responsible for drawing
         gamePanel = new Painter(player, map, shop);
 
+        //Define PlayerAnimation object, responsible for animating player character
+        playerAnimator = new PlayerAnimation(player, gamePanel);
+        
+        //detect pressing keys
+        gamePanel.addKeyListener(playerAnimator);
+
+        
         Opponent.informAboutPlayer(player);
         Opponent.informAboutGamePanel(gamePanel);
-        PlayerAnimation.informAboutGamePanel(gamePanel);
-        PlayerAnimation.informAboutPlayer(player);
-
-        ActionListener listenerForMovement = new PlayerAnimation();
-        Timer checkPlayerMovement = new Timer(50, listenerForMovement);
-        PlayerAnimation.informAboutTimerForMovement(checkPlayerMovement);
-        checkPlayerMovement.start();
     }
 
     public static void main(String[] args) {
@@ -47,9 +51,6 @@ public class SelfDefenceGame {
             frame = new JFrame("Self-defence Game");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(600, 600);
-
-            PlayerAnimation playerAnimation = new PlayerAnimation();
-            gamePanel.addKeyListener(playerAnimation);
 
             frame.add(gamePanel); 
             
