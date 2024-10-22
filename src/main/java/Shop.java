36075049b0d444e451d8d40d265f71f22d3c5b4a
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-22 16:27:38 +0200

    Add mesage to player and buying turret

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,15 +1,19 @@
 package src.main.java;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
  * Contains methods and variables relevant for in-game shop.
  */
-public class Shop {
+public class Shop implements ActionListener {
     private boolean isShown;
     private JButton buyTurretButton;
     private Player player;
+    private TurretManager turretManager;
+    private String messageToUser;
+    private Timer showMessageTimer = new Timer(1500, this);
 
     public boolean isShown() {
         return this.isShown;
@@ -26,14 +30,46 @@ public class Shop {
         isShown = true;
     }
 
+    private void setMessageToUser(String msg) {
+        messageToUser = msg;
+        showMessageTimer.start();
+    }
+
     /**
      * Constructor for Shop class.
      * @param p - Player object
+     * @param t - TurretManager object
      */
-    public Shop(Player p) {
+    public Shop(Player p, TurretManager t) {
         isShown = false;
-        buyTurretButton = new JButton(new ImageIcon("files/turretTest.png"));
+        buyTurretButton = new JButton(new ImageIcon("files/turret.png"));
         player = p;
+        turretManager = t;
+        messageToUser = "";
+    }
+
+    /**
+     * ActionListener for buying turrets.
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == showMessageTimer) {
+            setMessageToUser("");
+            showMessageTimer.stop();
+        }
+
+        if (e.getSource() == buyTurretButton) {
+            if (player.buy(Constants.getTurretPrice())) {
+                this.hideShop();
+                if (turretManager.placeTurret()) {
+                    this.showShop();
+                }
+            } else {
+                this.setMessageToUser(
+                    "Your balance is not sufficient! The turret costs " 
+                        + Constants.getTurretPrice());
+            }
+        }
     }
 
     /**
@@ -54,12 +90,20 @@ public class Shop {
             buyTurretButton.setBounds(20, Constants.getMapHeight(), 
                 20 + Constants.getTurretButtonWidth(), 
                     Constants.getTileWidht() + Constants.getTurretButtonHeight());
+            buyTurretButton.addActionListener(this);
             p.add(buyTurretButton);
 
             g.setColor(Color.BLACK);
             g.setFont(new Font("Arial", Font.PLAIN, 15));
             g2d.drawString("Your current balance: " + player.getCurrency(), 
                 300, Constants.getMapHeight() + 20);
+            
+            if (!"".equals(messageToUser)) {
+                g.setColor(Color.RED);
+                g.setFont(new Font("Arial", Font.BOLD, 15));
+                g2d.drawString(messageToUser, 
+                    300, Constants.getMapHeight() + 100);   
+            }
         
         } else {
             p.remove(buyTurretButton);
