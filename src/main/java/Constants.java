d4a8567c397d31029b5a7f1500a8148d4f6e1428
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-22 16:27:00 +0200

    Add constant rerlating to currency and turrets

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -60,7 +60,26 @@ public class Constants {
         return getMapHeight() + 150; 
     }
 
+    //CURRENCY
 
-    
-    
+    private static final long CURRENCY_FOR_SURVIVING_DEFEDN_PHASE = 10;
+
+    public static long getCurrencyForSurviving() {
+        return CURRENCY_FOR_SURVIVING_DEFEDN_PHASE;
+    }
+
+    //TURRETS
+
+    private static final long TURRET_PRICE = 10;
+    private static final int TURRET_HEALTH_POINTS = 20;
+
+    public static long getTurretPrice() {
+        return TURRET_PRICE;
+    }
+
+    public static int getTurretHealthPoints() {
+        return TURRET_HEALTH_POINTS;
+    }
+
+
 }
