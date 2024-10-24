cbcea1aff924e811091aaaff73e4a811e1a4e843
Hubert Łukasik <hubert.m.lukasik@gmail.com>
2024-10-24 15:54:17 +0200

    Add code to call bullet methods

--- src/main/java/TurretManager.java
+++ src/main/java/TurretManager.java
@@ -1,15 +1,20 @@
 package src.main.java;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
+import javax.swing.*;
 
 /**
  * Class for managing all the turrets on the screen.
  */
-public class TurretManager {
+public class TurretManager implements ActionListener {
 
     private ArrayList<Turret> turrets;
-    
+    private ArrayList<Bullet> bullets;
+    private Timer shootingTimer;
+    private Timer updateBulletPosition;
+
     /**
      * Get amount of all turrets in the game.
      * @return - amount of turrets
@@ -28,6 +33,40 @@ public class TurretManager {
         turrets.add(t);
     }
 
+    /**
+    TODO: add calculating angle. 
+    */
+    private void addBullet(int x, int y) {
+        Bullet b = new Bullet(x, y, 0);
+        bullets.add(b);
+    } 
+
+
+    /**
+     * ActionListener for genereting new bullets 
+     * in the game and updating position of existing ones.
+     * @param t - ActionEvent
+     */
+    @Override
+    public void actionPerformed(ActionEvent t) {
+        if (t.getSource() == shootingTimer) {
+            //shoot
+            for (Turret tur : turrets) {
+                addBullet(tur.getPosition()[0], tur.getPosition()[1]);
+            }
+        }
+
+        if (t.getSource() == updateBulletPosition) {
+            ArrayList<Bullet> bulletsStaying = new ArrayList<Bullet>();
+            for (Bullet b : bullets) {
+                if (b.updatePosition()) {
+                    bulletsStaying.add(b);
+                }
+            }
+
+            bullets = bulletsStaying;
+        }
+    }
 
     /**
      * Return 2-element array containing coordinates of the nearest turret to position (x, y). 
@@ -56,6 +95,10 @@ public class TurretManager {
      * Turn off all turrets.
      */
     public void startPlanPhase() {
+        shootingTimer.stop();
+        updateBulletPosition.stop();
+        bullets.clear();
+
         for (Turret t : turrets) {
             t.setOnline(false);
         }
@@ -65,6 +108,9 @@ public class TurretManager {
      * Turn on all turrets.
      */
     public void startDefendPhase() {
+        shootingTimer.start();
+        updateBulletPosition.start();
+
         for (Turret t : turrets) {
             t.setOnline(true);
         }
@@ -86,10 +132,19 @@ public class TurretManager {
         return counter;
     }
 
+    /**
+     * Draw method for the bullet.
+     * @param g - used by Swing
+     * @param p - game panel
+     */
     public void draw(Graphics g, Painter p) {
         for (Turret t: turrets) {
             t.draw(g, p);
-        }    
+        }
+        
+        for (Bullet b : bullets) {
+            b.draw(g, p);
+        }
     }
 
     
@@ -99,6 +154,9 @@ public class TurretManager {
     */
     public TurretManager() {
         this.turrets = new ArrayList<Turret>();
+        this.bullets = new ArrayList<Bullet>();
+        this.shootingTimer = new Timer(Constants.getTurretDelay(), this);
+        this.updateBulletPosition = new Timer(Constants.getBulletPositionUpdateDelay(), this);
     }
 
     
