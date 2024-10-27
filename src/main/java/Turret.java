fc0d72633890e2a869afe0ca9c1a034efd29f1c2
XxkuppiexX <37938611+XxkuppiexX@users.noreply.github.com>
2024-10-27 19:18:35 +0100

    Added collision to turrets

--- src/main/java/Turret.java
+++ src/main/java/Turret.java
@@ -44,7 +44,8 @@ public class Turret extends Entity {
      */
     public Turret(int x, int y) {
         this.setPosition(x, y);
-        this.setImage("turretExample");
+        this.setImage("turret");
         this.setHealthPoints(Constants.getTurretHealthPoints());
+        this.addCollision();
     }
 }
